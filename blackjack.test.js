f9c2e34de6f19da7002af7539eecde41c92ed5f7
fuzzyJess <jrundle99@yahoo.com>
2022-12-26 22:05:24 +0000

    passing test to check shuffleDeck function is changing order of passed in array

--- blackjack.test.js
+++ blackjack.test.js
@@ -7,13 +7,18 @@ describe("shuffleDeck() function", () => {
         const returnDeck = shuffleDeck(deck);
         expect(deck).not.toEqual(returnDeck);
     })
-    test('does not mutate the input array', () => {
+    test("does not mutate the input array", () => {
         const newDeck = [...deck];
         // creates new deck array which is a copy of the original deck
         shuffleDeck(newDeck);
         expect(newDeck).toEqual(deck);
         // the new deck array still matches the original deck array
-      });  
+    });
+    test("array returned is shuffled version of array passed in", () => {
+        const newArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        const returnArray = shuffleDeck(newArray);
+        expect(returnArray).not.toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    })  
 })
 describe("getCardsFromDeck() function", () => {
     test("returns card removed from end of deck", () => {
