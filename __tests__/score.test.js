b82580717a0ac85e3853d358633fa55a269b1fc9
fuzzyJess <jrundle99@yahoo.com>
2022-12-29 12:05:47 +0000

    created convertAces function to handle recalculating score when hand contains aces

--- __tests__/score.test.js
+++ __tests__/score.test.js
@@ -2,19 +2,26 @@ const { checkScore } = require('../score');
 
 describe("checkScore() function", () => {
     test("when passed a hand with a score of 21 or less returns correct score & validHand: true", () => {
-        const hand1 = [
+        const hand = [
             { name: '6 of Hearts', value: 6 },
             { name: '8 of Diamonds', value: 8 }
         ];
-        expect(checkScore(hand1)).toEqual({score: 14, validHand: true});
+        expect(checkScore(hand)).toEqual({score: 14, validHand: true});
     })
     test("when passed a hand with a score of over 21 returns correct score and validHand: false", () => {
-        const hand1 = [
+        const hand = [
             { name: '6 of Hearts', value: 6 },
             { name: '8 of Diamonds', value: 8 },
             { name: 'King of Spades', value: 10 }
         ];
-        expect(checkScore(hand1)).toEqual({score: 24, validHand: false});
-    
+        expect(checkScore(hand)).toEqual({score: 24, validHand: false});
+    })
+    test("when passed a hand with a score of over 21 but containing aces returns score with aces value reduced to 1", () => {
+        const hand = [
+            { name: 'Ace of Hearts', value: 11 },
+            { name: '8 of Diamonds', value: 8 },
+            { name: 'King of Spades', value: 10 }
+        ];
+        expect(checkScore(hand)).toEqual({score: 19, validHand: true});
     })
 })
--- score.js
+++ score.js
@@ -5,8 +5,26 @@ function checkScore(hand) {
         }
         if (handScore.score <= 21) {
             handScore.validHand = true;
+        } else if (handScore.score > 21) {
+            convertAces(hand, handScore);
         }
         return handScore;
 };
+
+function convertAces(hand, handScore) {
+    for (let i = 0; i < hand.length; i++) {
+        if (handScore.score > 21) {
+            if (hand[i].value === 11) {
+                handScore.score -= 10;
+                hand[i].value -= 10;
+                // removes 10 from overall score and 10 from value 
+                // of ace
+            }
+        }
+        if (handScore.score <= 21) {
+            handScore.validHand = true;
+        }
+    }
+}
 
 exporting: module.exports = { checkScore };
