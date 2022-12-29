4bc48c645716f07a71cbfb14158c3e3e9e5ba041
fuzzyJess <jrundle99@yahoo.com>
2022-12-29 21:37:44 +0000

    added tests for checkScore function which cover the scenarios regarding scoring

--- __tests__/score.test.js
+++ __tests__/score.test.js
@@ -1,7 +1,7 @@
 const { checkScore } = require('../score');
 
 describe("checkScore() function", () => {
-    test("when passed a hand with a score of 21 or less returns correct score & validHand: true", () => {
+    test("when passed a hand with a score of 21 or less returns correct score and validHand: true", () => {
         const hand = [
             { name: '6 of Hearts', value: 6 },
             { name: '8 of Diamonds', value: 8 }
@@ -16,12 +16,25 @@ describe("checkScore() function", () => {
         ];
         expect(checkScore(hand)).toEqual({score: 24, validHand: false});
     })
-    test("when passed a hand with a score of over 21 but containing aces returns score with aces value reduced to 1", () => {
+    test("when passed Blackjack returns correct score and vaildHand: true", () => {
         const hand = [
-            { name: 'Ace of Hearts', value: 11 },
-            { name: '8 of Diamonds', value: 8 },
-            { name: 'King of Spades', value: 10 }
+            { name: 'King of Spades', value: 10 },
+            { name: 'Ace of Diamonds', value: 11 }
         ];
-        expect(checkScore(hand)).toEqual({score: 19, validHand: true});
+        expect(checkScore(hand)).toEqual({score: 21, validHand: true});
+    })
+    test("when passed a hand with a score of over 21 but containing aces returns score with aces value reduced to 1", () => {
+        const hand1 = [
+            { name: 'King of Clubs', value: 10 },
+            { name: 'Queen of Diamonds', value: 10 },
+            { name: 'Ace of Hearts', value: 11 }
+        ];
+        expect(checkScore(hand1)).toEqual({score: 21, validHand: true});
+        const hand2 = [
+            { name: 'Nine of Hearts', value: 9 },
+            { name: 'Ace of Diamonds', value: 11 },
+            { name: 'Ace of Spades', value: 11 }
+        ];
+        expect(checkScore(hand2)).toEqual({score: 21, validHand: true});
     })
 })
--- score.js
+++ score.js
@@ -1,14 +1,14 @@
 function checkScore(hand) {
-    const handScore = {score: 0, validHand: false};
-        for (let i = 0; i < hand.length; i++) {
-            handScore.score += hand[i].value;
-        }
-        if (handScore.score <= 21) {
-            handScore.validHand = true;
-        } else if (handScore.score > 21) {
-            convertAces(hand, handScore);
-        }
-        return handScore;
+    const handScore = { score: 0, validHand: false };
+    for (let i = 0; i < hand.length; i++) {
+        handScore.score += hand[i].value;
+    }
+    if (handScore.score <= 21) {
+        handScore.validHand = true;
+    } else if (handScore.score > 21) {
+        convertAces(hand, handScore);
+    }
+    return handScore;
 };
 
 function convertAces(hand, handScore) {
