eafdb747375693369d94bd96baf09898fb5efc74
fuzzyJess <jrundle99@yahoo.com>
2023-01-01 16:21:48 +0000

    describing logic needed to play hand of blackjack

---
+++ game.js
@@ -0,0 +1,8 @@
+import inquirer from 'inquirer';
+
+// need to add new players to game using createPlayer for each new player
+// each player gets a hand dealt to them in turn
+// each player gets to chose to either stand or hit until either bust or standing
+// each player gets a message with their final score/whether they bust
+// dealer plays last - stands at 17 or above otherwise choosing hit
+// message at end to say who own that hand
