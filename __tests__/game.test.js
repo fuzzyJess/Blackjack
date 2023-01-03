7df9e1af51e22d4a12e8d94766be9222b3c64f58
fuzzyJess <jrundle99@yahoo.com>
2023-01-03 14:12:06 +0000

    separating out functionality for playing game to make code more readable and facilitate running tests

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -1,30 +1,30 @@
-const { gamePlayers, deck, newGame } = require('../game');
+const { gamePlayers, deck, startGame } = require('../game');
 
-describe("newGame() function", () => {
+describe("startGame() function", () => {
     test("uses array of players passed in to create new players needed for game", () => {
         const players = ["Jessica", "Rich"];
-        newGame(players, deck);
+        startGame(players, deck);
         expect(gamePlayers.length).toBe(2);
     });
-    // test("each player is dealt an initial hand", () => {
-    //     const players = ["Jessica", "Rich"];
-    //     newGame(players, deck);
-    //     expect(gamePlayers[0].hand.length).toBe(2);
-    //     expect(typeof gamePlayers[0].hand[0]).toBe("object");
-    // });
+    test("each player is dealt an initial hand", () => {
+        const players = ["Jessica", "Rich"];
+        startGame(players, deck);
+        expect(gamePlayers[0].hand.length).toBe(2);
+        expect(typeof gamePlayers[0].hand[0]).toBe("object");
+    });
     test("each player gets an initial score", () => {
         const players = ["Jessica", "Rich"];
-        newGame(players, deck);
+        startGame(players, deck);
         expect(typeof gamePlayers[0].currentScore).toBe("object");
     })
-    test("if player has a score of less than 17 they will get a new card", () => {
-        const players = ["Jessica"];
-        const deck1 = [
-            { name: '8 of Spades', value: 8 },
-            { name: '4 of Diamonds', value: 4 },
-            { name: 'Jack of Clubs', value: 10 }
-        ];
-        newGame(players, deck1);
-        expect(gamePlayers[4].hand.length).toBe(3);
-    })
+    // test("if player has a score of less than 17 they will get a new card", () => {
+    //     const players = ["Jessica"];
+    //     const deck1 = [
+    //         { name: '8 of Spades', value: 8 },
+    //         { name: '4 of Diamonds', value: 4 },
+    //         { name: 'Jack of Clubs', value: 10 }
+    //     ];
+    //     newGame(players, deck1);
+    //     expect(gamePlayers[4].hand.length).toBe(3);
+    // })
 });
--- game.js
+++ game.js
@@ -6,29 +6,31 @@ const { checkScore } = require('./score');
 
 // need to add new players to game using createPlayer for each new player
 // each player gets a hand dealt to them in turn
-// each player gets to chose to either stand or hit until either bust or standing
-// each player gets a message with their final score/whether they bust
+// each player gets to chose to either stand or hit until either bust or standing...
+    // could try to do this with a react app and promises...
+// each player gets a message with their final score/whether they are bust
 // dealer plays last - stands at 17 or above otherwise choosing hit
 // message at end to say who won that hand
 
 const gamePlayers = [];
 const deck = shuffleDeck(createDeck());
 
-function newGame(players, deck) {
+function startGame(players, deck) {
     players.forEach(player => {
         newPlayer = createPlayer(player);
         newPlayer.hand = deal(deck);
         newPlayer.currentScore = checkScore(newPlayer.hand);
-
-        do {
-            hit(newPlayer.hand, deck);
-            newPlayer.currentScore = checkScore(newPlayer.hand);
-        } while (newPlayer.currentScore.score < 17);
-        
         gamePlayers.push(newPlayer);
-                
     });
-       
 }
 
-exporting: module.exports = { gamePlayers, deck, newGame }
+function playGame(gamePlayers, deck) {
+
+    do {
+        hit(newPlayer.hand, deck);
+        newPlayer.currentScore = checkScore(newPlayer.hand);
+    } while (newPlayer.currentScore.score < 17);
+    
+}
+
+exporting: module.exports = { gamePlayers, deck, startGame, playGame }
