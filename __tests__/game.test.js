7402deab7569f4c88026826dcceca3a659c07091
fuzzyJess <jrundle99@yahoo.com>
2023-01-06 14:56:52 +0000

    restructure tests for game.js

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -1,57 +1,47 @@
 const { createDeck } = require('../deck');
 const { shuffleDeck } = require('../deal');
-const { playGame, addPlayers, dealCards, hitExtraCards, evaluateGame } = require('../game');
+const { playGame, dealCards, hitExtraCards, evaluateGame } = require('../game');
 const { createPlayer } = require('../player');
-
-describe("addPlayers() function", () => {
-    test("creates array containing new player and dealer objects", () => {
-        newDeck = shuffleDeck(createDeck());
-        let players = addPlayers("Jessica");
-        expect(players[0]).toEqual({ playerName: "Jessica" });
-    });
-});
 
 describe("dealCards() function", () => {
     test("deals initial hand to players", () => {
         newDeck = shuffleDeck(createDeck());
-        let players = addPlayers("Jessica");
-        dealCards(players, newDeck);
-        expect(players[0].hand.length).toBe(2);
-        expect(typeof players[0].hand[0]).toBe("object");
-        expect(players[1].hand.length).toBe(2);
-        expect(typeof players[1].hand[0]).toBe("object");
+        let player = createPlayer("Jessica");
+        dealCards(player, newDeck);
+        expect(player.hand.length).toBe(2);
+        expect(typeof player.hand[0]).toBe("object");
     })
 })
 
 describe("hitExtraCards", () => {
     test("only deals extra cards to players who's score is below 17", () => {
         newDeck = shuffleDeck(createDeck());
-        let players = [{
+        let player = {
             playerName: 'Jessica',
             hand: [
               { name: '7 of Hearts', value: 7 },
               { name: '4 of Diamonds', value: 4 }
             ],
             currentScore: { score: 11, validHand: true }
-          }, 
-          {
+          };
+          let dealer = {
             playerName: 'Dealer Dan',
             hand: [
               { name: 'Queen of Spades', value: 10 },
               { name: 'King of Spades', value: 10 }
             ],
             currentScore: { score: 20, validHand: true }
-          }];
-        hitExtraCards(players, newDeck);
-        expect(players[0].hand.length > 2).toBe(true);
-        expect(players[1].hand.length).toBe(2);
+          };
+        hitExtraCards(player, newDeck);
+        expect(player.hand.length > 2).toBe(true);
+        hitExtraCards(dealer, newDeck);
+        expect(dealer.hand.length).toBe(2);
     })
 })
 
 describe("evaluateGame", () => {
     test("Console.log should have been called with correct end game message", () => {
-        let players = [
-            {
+        let player = {
             playerName: 'Jessica',
             hand: [
                 { name: 'King of Spades', value: 10 },
@@ -59,8 +49,8 @@ describe("evaluateGame", () => {
                 { name: '7 of Hearts', value: 7 }
             ],
             currentScore: { score: 21, validHand: true }
-            },
-            {
+            };
+            let dealer = {
             playerName: 'Dealer Dan',
             hand: [
                 { name: '8 of Clubs', value: 8 },
@@ -68,10 +58,9 @@ describe("evaluateGame", () => {
                 { name: '8 of Spades', value: 8 }
             ],
             currentScore: { score: 20, validHand: true }
-            }
-        ];
+            };
         const logSpy = jest.spyOn(global.console, 'log');
-        evaluateGame(players);
+        evaluateGame(dealer, player);
         expect(logSpy).toHaveBeenCalledTimes(1);
         expect(logSpy).toHaveBeenCalledWith('Jessica has won!');
 
