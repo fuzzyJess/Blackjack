fdcaf46bc4a7a03f709102335cd9cb67c5a440e3
fuzzyJess <jrundle99@yahoo.com>
2023-01-07 18:27:03 +0000

    remove import for inquirer and add missing semicolons

--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-// import inquirer from 'inquirer';
 const { createDeck } = require('./deck');
 const { createPlayer, dealerHit, playerHit } = require('./player');
 const { deal, shuffleDeck } = require('./deal');
@@ -14,31 +13,31 @@ function playGame(newPlayer) {
     playerHit(player, deck);
     dealerHit(dealer, deck);
     evaluateGame(dealer, player);
-}
+};
 
 function dealCards(player, deck) {
     player.hand = deal(deck);
     player.currentScore = checkScore(player.hand);
     console.log(`${player.playerName} was dealt the ${player.hand[0].name} and the ${player.hand[1].name}. ${player.playerName}'s current score is ${player.currentScore.score}.`)
-}
+};
 
 function evaluateGame(dealer, player) {
     let winner = "";
     if (player.currentScore.validHand === true) {
         if (player.currentScore.score > dealer.currentScore.score || dealer.currentScore.validHand === false) {
             winner = player.playerName;
-        } 
-    } 
+        }; 
+    }; 
     if (dealer.currentScore.validHand === true) {
         if (dealer.currentScore.score > player.currentScore.score || player.currentScore.validHand === false) {
             winner = dealer.playerName;
-        } 
-    }
+        }; 
+    };
     if (winner.length > 0) {
         console.log(`${winner} has won!`)
     } else {
         console.log("It was a tie!")
-    }
-}
+    };
+};
 
 exporting: module.exports = { playGame, dealCards, evaluateGame }
