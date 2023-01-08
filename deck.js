bfd2524e8c54e3a4334a850d4a60d94e2661aab8
fuzzyJess <jrundle99@yahoo.com>
2023-01-08 16:35:05 +0000

    refactor createDeck function

--- deck.js
+++ deck.js
@@ -1,43 +1,39 @@
 function createDeck () {
+  const suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs']
+  const cards = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+
   const deck = []
 
-  for (let j = 0; j < 4; j++) {
-    let suit = ''
-    let card = ''
-    let cardValue = 0
-    if (j === 0) {
-      suit = 'Spades'
-    }
-    if (j === 1) {
-      suit = 'Hearts'
-    }
-    if (j === 2) {
-      suit = 'Diamonds'
-    }
-    if (j === 3) {
-      suit = 'Clubs'
-    }
-    for (let i = 0; i < 13; i++) {
-      if (i === 0) {
-        card = 'Ace of ' + suit
-        cardValue = 11
-      } else if (i === 10) {
-        card = 'Jack of ' + suit
-        cardValue = 10
-      } else if (i === 11) {
-        card = 'Queen of ' + suit
-        cardValue = 10
-      } else if (i === 12) {
-        card = 'King of ' + suit
-        cardValue = 10
-      } else {
-        card = `${i + 1} of ${suit}`
-        cardValue = i + 1
+  suits.forEach(suit => {
+    cards.forEach(card => {
+      let name
+      let value
+      switch (card) {
+        case 'A':
+          name = `Ace of ${suit}`
+          value = 11
+          break
+        case 'J':
+          name = `Jack of ${suit}`
+          value = 10
+          break
+        case 'Q':
+          name = `Queen of ${suit}`
+          value = 10
+          break
+        case 'K':
+          name = `King of ${suit}`
+          value = 10
+          break
+        default:
+          name = `${card} of ${suit}`
+          value = card
       }
+      deck.push({ name, value })
+    })
+  })
 
-      deck.push({ name: card, value: cardValue })
-    }
-  }
   return deck
 }
-exporting: module.exports = { createDeck }
+
+module.exports = { createDeck }
